f2f67cebd98581915c0ab06a4f5993e70c31195c
Alex <abreskolenko@yandex.ru>
2024-07-10 22:25:28 +0300

    Task 1 completed

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
+//********** Task 1
+// for (let i = 0; i < 11; i++) {
+//   if (i === 0) {
+//     console.log(`${i} - это ноль`);
+//   } else if (i % 2 === 0) {
+//     console.log(`${i} - четное число`);
+//   } else {
+//     console.log(`${i} - нечетное число`);
+//   }
+// }
 
+// ********Task 2
+// const arrayNumber = [1, 2, 3, 4, 5, 6, 7];
+// let lastNumber = arrayNumber.pop();
